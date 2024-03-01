615d8df02682319f0c47f8e76ddfaf36408ea470
SHIFU <tamangnischal2018@gmail.com>
2024-03-01 20:38:09 +0545

    profile icon and name

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { navigationMenu } from "./SidebarNavigation";
+import { Avatar, Divider } from "@mui/material";
 
 const Sidebar = () => {
   return (
@@ -13,6 +14,19 @@ const Sidebar = () => {
           <p className="text-xl">{item.title}</p>
         </div>)}</div>
       </div>
+    <div>
+      <Divider/ > 
+      <div className="pl-5 flex items-center justify-between pt-5">
+        <div className="flex items-center space-x-3 ">
+          <Avatar src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_960_720.png"/>
+         <div>
+          <p>Nischal tamang</p>
+         </div>
+          
+        </div>
+      </div>
+     </div>
+  
     </div>
   );
 };
