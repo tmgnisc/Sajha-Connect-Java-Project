c3a25acb50ec5f049dab37b620443bb724039ea4
SHIFU <tamangnischal2018@gmail.com>
2024-03-02 12:49:42 +0545

    status bar

--- src/components/MiddlePart/MiddlePart.jsx
+++ src/components/MiddlePart/MiddlePart.jsx
@@ -1,10 +1,17 @@
-import { Avatar, Card } from "@mui/material";
+import { Avatar, Card, IconButton } from "@mui/material";
 import React from "react";
 import AddIcon from "@mui/icons-material/Add";
 import StoryCircle from "./StoryCircle";
+import ImageIcon from "@mui/icons-material/Image";
+import VideocamIcon from '@mui/icons-material/Videocam';
+import ArticleIcon from '@mui/icons-material/Article';
 
 const story = [11, 1, 1, 1, 1];
 const MiddlePart = () => {
+  const handleOpenCreatePostModal = () => {
+    console.log("hello this is handleopen");
+  };
+
   return (
     <div className="px-20">
       <section className="flex items-center p-5 rounded-b-md">
@@ -22,7 +29,34 @@ const MiddlePart = () => {
       <Card className="p-5 mt-5">
         <div className="flex justify-between">
           <Avatar />
-          <input className="outline-none w-[90%] rounded-full px-5 bg-transparent border-[#3b4054] border" type="text"/>
+          <input
+            readOnly
+            className="outline-none w-[90%] rounded-full px-5 bg-transparent border-[#3b4054] border"
+            type="text"
+          />
+        </div>
+        <div className="flex justify-center space-x-9 mt-5">
+          <div className="flex items-center">
+            <IconButton color="primary" onClick={handleOpenCreatePostModal}>
+              <ImageIcon />
+            </IconButton>
+            <span>media</span>
+          </div>
+          <div className="flex items-center">
+            <IconButton color="primary" onClick={handleOpenCreatePostModal}>
+   
+            <VideocamIcon/>
+            </IconButton>
+            <span>Video</span>
+          </div>
+          <div className="flex items-center">
+            <IconButton color="primary" onClick={handleOpenCreatePostModal}>
+   
+            <ArticleIcon/>
+            </IconButton>
+            <span> Write Article</span>
+          </div>
+
         </div>
       </Card>
     </div>
