8974225c81207de4eccfa84d1ae0ce434e08c5d1
SHIFU <tamangnischal2018@gmail.com>
2024-03-02 13:26:49 +0545

    working in post bar

--- src/components/Post/PostCard.jsx
+++ src/components/Post/PostCard.jsx
@@ -1,12 +1,27 @@
-import { Avatar, Card, CardHeader, CardMedia, IconButton } from "@mui/material";
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@mui/material";
 import { red } from "@mui/material/colors";
 import React from "react";
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import ShareIcon from '@mui/icons-material/Share';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
 
 const PostCard = () => {
   return (
     <Card className="">
-    <CardHeader
+      <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
             R
@@ -17,15 +32,41 @@ const PostCard = () => {
             <MoreVertIcon />
           </IconButton>
         }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title="Nischal"
+        subheader="@nichaltmg"
       />
-       <CardMedia
+      <CardMedia
         component="img"
         height="194"
-        image="https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
+        image="https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
         alt="Paella dish"
       />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.
+        </Typography>
+      </CardContent>
+
+      <CardActions className="flex justify-between" disableSpacing>
+        <div>
+          <IconButton>
+            {true ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+          </IconButton>
+          <IconButton>
+            <ShareIcon/>
+          </IconButton>
+
+          <IconButton>
+            <ChatBubbleIcon/>
+          </IconButton>
+        </div>
+      </CardActions>
+
+      <div>
+         {true?<BookmarkIcon/>:<BookmarkBorderIcon/>}
+      </div>
     </Card>
   );
 };
