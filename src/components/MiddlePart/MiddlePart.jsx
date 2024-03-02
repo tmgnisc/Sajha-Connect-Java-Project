14a0269d84989602178b548ef116a0c88f9c0d2c
SHIFU <tamangnischal2018@gmail.com>
2024-03-02 12:39:44 +0545

    working on status bar

--- src/components/MiddlePart/MiddlePart.jsx
+++ src/components/MiddlePart/MiddlePart.jsx
@@ -1,22 +1,30 @@
-import { Avatar } from "@mui/material";
+import { Avatar, Card } from "@mui/material";
 import React from "react";
 import AddIcon from "@mui/icons-material/Add";
 import StoryCircle from "./StoryCircle";
 
-const story = [11,1,1,1,1];
+const story = [11, 1, 1, 1, 1];
 const MiddlePart = () => {
   return (
     <div className="px-20">
-      <div className="flex items-center p-5 rounded-b-md">
+      <section className="flex items-center p-5 rounded-b-md">
         <div className="flex flex-col items-center mr-4 cursor-pointer">
           <Avatar sx={{ width: "5rem", height: "5rem" }}>
             <AddIcon sx={{ fontSize: "3rem" }} />
           </Avatar>
           <p>New</p>
         </div>
-{story.map((item)=><StoryCircle/>)}
-        
-      </div>
+        {story.map((item) => (
+          <StoryCircle />
+        ))}
+      </section>
+
+      <Card className="p-5 mt-5">
+        <div className="flex justify-between">
+          <Avatar />
+          <input className="outline-none w-[90%] rounded-full px-5 bg-transparent border-[#3b4054] border" type="text"/>
+        </div>
+      </Card>
     </div>
   );
 };
