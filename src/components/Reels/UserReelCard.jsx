0ef63b47a6b11680d228847a09ff450e324cd71d
SHIFU <tamangnischal2018@gmail.com>
2024-03-04 15:07:37 +0545

    user profile save card

--- src/components/Reels/UserReelCard.jsx
+++ src/components/Reels/UserReelCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const UserReelCard = () => {
   return (
     <div className='w-[15rem] px-2'>
-    <video className='w-full h-full' src='https://player.vimeo.com/external/537145302.sd.mp4?s=a2cb22a1b179c7c3c797b9b4d2a3707c28101da7&profile_id=165&oauth2_token_id=57447761'/>
+    <video controls className='w-full h-full' src='https://player.vimeo.com/external/537145302.sd.mp4?s=a2cb22a1b179c7c3c797b9b4d2a3707c28101da7&profile_id=165&oauth2_token_id=57447761'/>
     </div>
   )
 }
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -13,6 +13,7 @@ const tabs = [
 
 const posts = [1, 1, 1, 1];
 const reels = [1, 1, 1, 1];
+const savedPost = [1,1,1,1]
 const Profile = () => {
   const { id } = useParams();
   const [value, setValue] = React.useState("post");
@@ -84,12 +85,18 @@ const Profile = () => {
                 ))}
               </div>
             ) : value === "reels" ? (
-              <div className="flex flex-wrap gap-2">
+              <div className="flex justify-center flex-wrap gap-2 my-10 ">
                 {reels.map((item) => (
                   <UserReelCard />
                 ))}
               </div>
-            ) : (
+            ):value==="saved"?   <div className="space-y-5 w-[70%] my-10">
+            {posts.map((item) => (
+              <div className="border border-slate-100 rounded-md ">
+                <PostCard />
+              </div>
+            ))}
+          </div> : (
               ""
             )}
           </div>
