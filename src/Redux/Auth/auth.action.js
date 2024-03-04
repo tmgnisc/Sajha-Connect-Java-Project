40e20f17f77f4777e1c8a5414c39681157e10bfa
SHIFU <tamangnischal2018@gmail.com>
2024-03-04 20:29:56 +0545

    getprofileaction

--- src/Redux/Auth/auth.action.js
+++ src/Redux/Auth/auth.action.js
@@ -1,40 +1,66 @@
-import axios from "axios"
-import { API_BASE_URL } from "../../config/api"
-import { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS } from "./auth.actionType"
+import axios from "axios";
+import { API_BASE_URL } from "../../config/api";
+import {
+  GET_PROFILE_FAILURE,
+  GET_PROFILE_REQUEST,
+  GET_PROFILE_SUCCESS,
+  LOGIN_FAILURE,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+} from "./auth.actionType";
 
-export const LoginUserAction=(LoginData)=>async(dispatch)=>{
-    dispatch({type:LOGIN_REQUEST})
-    try{
-        const{data}=await axios.post(`${API_BASE_URL}/auth/signin`, LoginData.data)
+export const LoginUserAction = (LoginData) => async (dispatch) => {
+  dispatch({ type: LOGIN_REQUEST });
+  try {
+    const { data } = await axios.post(
+      `${API_BASE_URL}/auth/signin`,
+      LoginData.data
+    );
 
-        if(data.jwt){
-            localStorage.setItem("jwt", data.jwt)
-           
-        }
-        dispatch({type:LOGIN_SUCCESS, payload:data.jwt})
-    } catch (error) {
-
-        console.log("----------", error);
-        dispatch({type:LOGIN_FAILURE, payload:error})
+    if (data.jwt) {
+      localStorage.setItem("jwt", data.jwt);
     }
-}
+    dispatch({ type: LOGIN_SUCCESS, payload: data.jwt });
+  } catch (error) {
+    console.log("----------", error);
+    dispatch({ type: LOGIN_FAILURE, payload: error });
+  }
+};
 
-export const registerUserAction=(LoginData)=>async(dispatch)=>{
-    dispatch({type:LOGIN_REQUEST})
-    try{
-        const{data}=await axios.post(`${API_BASE_URL}/auth/signup`, LoginData.data)
+export const registerUserAction = (LoginData) => async (dispatch) => {
+  dispatch({ type: LOGIN_REQUEST });
+  try {
+    const { data } = await axios.post(
+      `${API_BASE_URL}/auth/signup`,
+      LoginData.data
+    );
 
-        if(data.jwt){
-            localStorage.setItem("jwt", data.jwt)
-            console.log("login vayo hai ta", data)
-           
-        }
-
-        console.log("register vayo hai ta ", data)
-        dispatch({type:LOGIN_SUCCESS, payload:data.jwt})
-    } catch (error) {
-
-        console.log("----------", error);
-        dispatch({type:LOGIN_FAILURE, payload:error})
+    if (data.jwt) {
+      localStorage.setItem("jwt", data.jwt);
+      console.log("login vayo hai ta", data);
     }
-}
+
+    console.log("register vayo hai ta ", data);
+    dispatch({ type: LOGIN_SUCCESS, payload: data.jwt });
+  } catch (error) {
+    console.log("----------", error);
+    dispatch({ type: LOGIN_FAILURE, payload: error });
+  }
+};
+
+export const getProfileAction = (jwt) => async (dispatch) => {
+  dispatch({ type: GET_PROFILE_REQUEST });
+  try {
+    const { data } = await axios.post(`${API_BASE_URL}/auth/users/profile`, {
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    });
+
+    console.log("profile setup ", data);
+    dispatch({ type: GET_PROFILE_SUCCESS, payload: data });
+  } catch (error) {
+    console.log("----------", error);
+    dispatch({ type: GET_PROFILE_FAILURE, payload: error });
+  }
+};
