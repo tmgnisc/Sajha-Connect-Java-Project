81285d88b04431c0cc5578e7789bb1d8bd7659ae
SHIFU <tamangnischal2018@gmail.com>
2024-03-27 15:56:50 +0545

    bug fixed controller

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -3,12 +3,15 @@ package com.example.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.User;
 
 @RestController
+@ComponentScan("com.example")	
 public class UserController {
 
 	
@@ -21,6 +24,20 @@ public class UserController {
 		
 		users.add(user1);
 		return users;
+		
+	}
+	
+		@GetMapping("/users/{userId}")
+		public User getUserById(@PathVariable("userId")int id) {
+		
+	
+		User user1 = new User(1,"Nischal","Tamang","nschaltmg2023@gmail.com","12345" );
+		user1.setId(id);
+		
+
+		
+	
+		return user1;
 		
 	}
 }
