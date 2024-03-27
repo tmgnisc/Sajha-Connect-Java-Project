aee716cab33dacbe0037cf0ff26c5c6705653d25
SHIFU <tamangnischal2018@gmail.com>
2024-03-27 13:49:39 +0545

    working on controller and model

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,14 +1,26 @@
 package com.example.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.User;
 
 @RestController
 public class UserController {
 
 	
 	@GetMapping("/users")
-	public String getUsers() {
-		return "get all users";
+	public List<User> getUsers() {
+		
+		List<User> users = new ArrayList<>();
+		User user1 = new User(1,"Nischal","Tamang","nschaltmg2023@gmail.com","12345" );
+
+		
+		users.add(user1);
+		return users;
+		
 	}
 }
--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -2,7 +2,7 @@ package com.example.model;
 
 public class User {
 
-	
+	private int id;
 	private String firstName;
 	private String lastName;
 	private String email;
@@ -12,13 +12,30 @@ public class User {
 		// TODO Auto-generated constructor stub
 	}
 
-	public User(String firstName, String lastName, String email, String password) {
+
+
+	public User(int id, String firstName, String lastName, String email, String password) {
 		super();
+		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.password = password;
 	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
 
 	public String getFirstName() {
 		return firstName;
