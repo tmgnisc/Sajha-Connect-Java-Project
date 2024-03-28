bc27e1d6acc869ba565f623f06d66c80a21dfcb7
SHIFU <tamangnischal2018@gmail.com>
2024-03-28 11:48:01 +0545

    table and connection with database

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -1,7 +1,15 @@
 package com.example.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
 public class User {
 
+	@Id
+	
 	private int id;
 	private String firstName;
 	private String lastName;
