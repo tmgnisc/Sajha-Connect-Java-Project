5714718264ecd35b901ef308537d38822ec93c6f
SHIFU <tamangnischal2018@gmail.com>
2024-03-28 11:50:26 +0545

    repository create

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.example.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
