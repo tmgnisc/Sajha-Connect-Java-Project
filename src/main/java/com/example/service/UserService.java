376b268f46ff645ee8636e11925d2f6641382f76
SHIFU <tamangnischal2018@gmail.com>
2024-03-29 11:58:46 +0545

    service interface

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.service;
 
+import java.util.List;
+
 import com.example.model.User;
 
 public interface UserService {
@@ -8,5 +10,7 @@ public interface UserService {
 	public User registerUser(User user);
 	public User finUserById(int userId);
 	public User findUserByEmail(String email);
-    public User followUser();
+    public User followUser(int userId1, int userId2);
+    public User updateUser(User user);
+    public List<User> searchuser(String query);
 }
