60a7c474ae8cd02bc374849d6184cc8e32c82521
SHIFU <tamangnischal2018@gmail.com>
2024-03-29 11:51:19 +0545

    user api backend

---
+++ src/main/java/com/example/UserService.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import com.example.model.User;
+
+public interface UserService {
+
+	
+	public User registerUser(User user);
+	public User finUserById(int userId);
+	public User findUserByEmail(String email);
+}
