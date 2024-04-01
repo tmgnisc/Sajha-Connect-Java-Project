a49c6f612ed39a12d162c058d5a9ddd3557862f6
SHIFU <tamangnischal2018@gmail.com>
2024-04-01 12:14:44 +0545

    delete method post

--- src/main/java/com/example/controller/PostController.java
+++ src/main/java/com/example/controller/PostController.java
@@ -3,7 +3,9 @@ package com.example.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.Post;
@@ -18,10 +20,18 @@ public class PostController {
 	
 	
 	//j pani response pathauxam frontend ma responseentity bata pathauxam direct class bata haina
-	@PostMapping("/posts")
-	public ResponseEntity<Post> createPost(Post post, int userId)throws Exception{
+	@PostMapping("/posts/user/{userId}")  //userid yesma logged in bata linxa not from frontend
+	public ResponseEntity<Post> createPost(@RequestBody Post post, @PathVariable int userId)throws Exception{
 		
 		Post createdPost = postService.createNewPost(post, userId);
 		return new ResponseEntity<>(createdPost, HttpStatus.ACCEPTED);
+	}
+	
+	//delete post method
+	
+	public ResponseEntity<String> deletePost(@PathVariable int postId, @PathVariable int userId){
+		
+		
+		return null;
 	}
 }
