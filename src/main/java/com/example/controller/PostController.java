dde7419a8f5f3a0ff5ea61667a4bf2d6c56f6dfa
SHIFU <tamangnischal2018@gmail.com>
2024-04-01 12:11:10 +0545

    postmapping posts

--- src/main/java/com/example/controller/PostController.java
+++ src/main/java/com/example/controller/PostController.java
@@ -1,8 +1,12 @@
 package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.model.Post;
 import com.example.service.PostService;
 
 @RestController
@@ -10,4 +14,14 @@ public class PostController {
 
 	@Autowired
 	PostService postService;
+	
+	
+	
+	//j pani response pathauxam frontend ma responseentity bata pathauxam direct class bata haina
+	@PostMapping("/posts")
+	public ResponseEntity<Post> createPost(Post post, int userId)throws Exception{
+		
+		Post createdPost = postService.createNewPost(post, userId);
+		return new ResponseEntity<>(createdPost, HttpStatus.ACCEPTED);
+	}
 }
