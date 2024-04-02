f44dda9002630998d138ce8d185987aad77b4df3
SHIFU <tamangnischal2018@gmail.com>
2024-04-02 16:35:00 +0545

    making custom jwt

--- src/main/java/com/example/service/CustomUserDetailsService.java
+++ src/main/java/com/example/service/CustomUserDetailsService.java
@@ -1,13 +1,19 @@
 package com.example.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.example.model.User;
 import com.example.repository.UserRepository;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
 	@Autowired
@@ -20,7 +26,12 @@ public class CustomUserDetailsService implements UserDetailsService {
 		if(user == null) {
 			throw new UsernameNotFoundException("user not found with email +" +username);
 		}
-		return null;
+		
+		//user role xaina tesaile empty xa role user ko role define gareko xaina 
+		//ecommerce tira chai hunax yesto end point accecss dinu parxa aile hamro project ma chai xaina
+		List<GrantedAuthority> authorities=new ArrayList<>();
+		
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
 	}
 
 	
