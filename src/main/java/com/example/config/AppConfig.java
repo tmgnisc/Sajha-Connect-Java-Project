ae39181a918926c3070a88bb25c738fba80893b2
SHIFU <tamangnischal2018@gmail.com>
2024-04-02 15:30:54 +0545

    configuring spring security

---
+++ src/main/java/com/example/config/AppConfig.java
@@ -0,0 +1,18 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+	
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+	
+		
+		return http.build();
+	}
+}
