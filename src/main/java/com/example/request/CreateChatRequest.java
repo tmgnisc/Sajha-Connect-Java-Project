b1437b48f92775893af212e9702b5a7a6af18b25
SHIFU <tamangnischal2018@gmail.com>
2024-04-04 21:03:09 +0545

    chatapi complete

--- src/main/java/com/example/request/CreateChatRequest.java
+++ src/main/java/com/example/request/CreateChatRequest.java
@@ -5,7 +5,7 @@ import com.example.model.User;
 public class CreateChatRequest {
 	
 	private User reqUser;
-	
+	private int userId;
  private User user2;
  
  public CreateChatRequest() {
@@ -28,11 +28,22 @@ public void setUser2(User user2) {
 	this.user2 = user2;
 }
 
-public CreateChatRequest(User reqUser, User user2) {
+public int getUserId() {
+	return userId;
+}
+
+public void setUserId(int userId) {
+	this.userId = userId;
+}
+
+public CreateChatRequest(User reqUser, int userId, User user2) {
 	super();
 	this.reqUser = reqUser;
+	this.userId = userId;
 	this.user2 = user2;
 }
+
+
  
  
 }
