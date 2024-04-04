d4a9b035dea30341b01c9711e4e46abd2ce54494
SHIFU <tamangnischal2018@gmail.com>
2024-04-04 15:16:50 +0545

    like comment controller

--- src/main/java/com/example/controller/CommentController.java
+++ src/main/java/com/example/controller/CommentController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +31,16 @@ public class CommentController {
 		Comment createdComment = commentService.createComment(comment, postId, user.getId());
 		
 		return createdComment;
+		
+	}
+	
+	@PutMapping("/api/comments/like/{commentId}")
+	public Comment likeComment(@RequestHeader("Authorization") String jwt, @PathVariable int commentId) throws Exception {
+		
+		User user = userService.findUserByJwt(jwt);
+		Comment likedComment = commentService.likeComment(commentId, user.getId());
+		
+		return likedComment;
 		
 	}
 }
