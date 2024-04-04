bedfc70cb32c321714d160d1a6e675cd6c3b49f8
SHIFU <tamangnischal2018@gmail.com>
2024-04-04 12:04:10 +0545

    comment model complete

--- src/main/java/com/example/model/Comment.java
+++ src/main/java/com/example/model/Comment.java
@@ -1,6 +1,7 @@
 package com.example.model;
 
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,5 +29,68 @@ public class Comment {
 	//comment lai multiple user le like garna sakxan
 	@ManyToMany
 	private List<User> liked = new ArrayList<>();
+	
+	private LocalDateTime createdAt;
+	
+	
+	
+	public Comment() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public Comment(int id, String content, User user, List<User> liked, LocalDateTime createdAt) {
+		super();
+		this.id = id;
+		this.content = content;
+		this.user = user;
+		this.liked = liked;
+		this.createdAt = createdAt;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public List<User> getLiked() {
+		return liked;
+	}
+
+	public void setLiked(List<User> liked) {
+		this.liked = liked;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	
 	
 }
