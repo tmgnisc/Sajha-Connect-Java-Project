f7a2b5a4ba7cef1fd891dc9af7941c90611027d2
SHIFU <tamangnischal2018@gmail.com>
2024-04-04 16:05:41 +0545

    reels api model

---
+++ src/main/java/com/example/model/Reels.java
@@ -0,0 +1,79 @@
+package com.example.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+
+public class Reels {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String title;
+	private String video;
+	
+	//euta user le dherai reels banauna milxa tara euta reels euta matra user sita relate hunxa
+	@ManyToOne
+	private User user;
+	
+	
+	public Reels() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Reels(Long id, String title, String video, User user) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.video = video;
+		this.user = user;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getVideo() {
+		return video;
+	}
+
+
+	public void setVideo(String video) {
+		this.video = video;
+	}
+
+
+	public User getUser() {
+		return user;
+	}
+
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	
+}
