49037a06afdf553edbece4dfa5b23f81aba35ddd
SHIFU <tamangnischal2018@gmail.com>
2024-04-05 14:19:15 +0545

    message API model

--- src/main/java/com/example/model/Chat.java
+++ src/main/java/com/example/model/Chat.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Chat {
@@ -27,18 +28,25 @@ public class Chat {
 	
 	private LocalDateTime timestamp;
 	
+	@OneToMany(mappedBy = "chat")   //yo garyo vane yesle database ma arko table create gardaina
+	private List<Message> messages = new ArrayList<>();
+	
 	public Chat() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Chat(int id, String chat_name, String chat_image, List<User> users, LocalDateTime timestamp) {
+
+	public Chat(int id, String chat_name, String chat_image, List<User> users, LocalDateTime timestamp,
+			List<Message> messages) {
 		super();
 		this.id = id;
 		this.chat_name = chat_name;
 		this.chat_image = chat_image;
 		this.users = users;
 		this.timestamp = timestamp;
+		this.messages = messages;
 	}
+
 
 	public int getId() {
 		return id;
@@ -78,6 +86,16 @@ public class Chat {
 
 	public void setTimestamp(LocalDateTime timestamp) {
 		this.timestamp = timestamp;
+	}
+
+
+	public List<Message> getMessages() {
+		return messages;
+	}
+
+
+	public void setMessages(List<Message> messages) {
+		this.messages = messages;
 	}
 	
 	
---
+++ src/main/java/com/example/model/Message.java
@@ -0,0 +1,92 @@
+package com.example.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Message {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String content;
+	private String image;
+	
+	@ManyToOne
+	private User user;
+	
+	@ManyToOne
+	private Chat chat;
+	
+	private LocalDateTime timestamp;
+	
+	public Message() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Message(int id, String content, String image, User user, Chat chat, LocalDateTime timestamp) {
+		super();
+		this.id = id;
+		this.content = content;
+		this.image = image;
+		this.user = user;
+		this.chat = chat;
+		this.timestamp = timestamp;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Chat getChat() {
+		return chat;
+	}
+
+	public void setChat(Chat chat) {
+		this.chat = chat;
+	}
+
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+	
+}
