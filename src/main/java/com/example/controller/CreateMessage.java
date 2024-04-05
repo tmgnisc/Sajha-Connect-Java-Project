9a71addd9c1c9c49e13277cf3bf100a890d4f624
SHIFU <tamangnischal2018@gmail.com>
2024-04-05 14:43:06 +0545

    message API controller

--- src/main/java/com/example/controller/CreateMessage.java
+++ src/main/java/com/example/controller/CreateMessage.java
@@ -1,5 +1,43 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Message;
+import com.example.model.User;
+import com.example.service.MessageService;
+import com.example.service.UserService;
+
+@RestController
 public class CreateMessage {
 
+	@Autowired
+	private MessageService messageService;
+	
+	@Autowired
+	private UserService userService;
+	
+	
+	@PostMapping("/api/messages/chat/{chatId}")
+	public Message createMessage(@RequestBody Message req, 
+			@RequestHeader("Authorization") String jwt, @PathVariable int chatId) throws Exception {
+		User user = userService.findUserByJwt(jwt);
+		Message message = messageService.createMessage(user, chatId, req);
+		return message;
+	}
+	
+	
+	@GetMapping("/api/messages/chat/{chatId}")
+	public List<Message> createMessage(@RequestHeader("Authorization") String jwt, @PathVariable int chatId) throws Exception {
+		User user = userService.findUserByJwt(jwt);
+		List<Message> messages = messageService.findChatsMessages(chatId);
+		return messages;
+	}
 }
