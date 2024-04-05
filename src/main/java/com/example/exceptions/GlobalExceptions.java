317beed3981cf37a9b0856038ef5cef611ac282d
SHIFU <tamangnischal2018@gmail.com>
2024-04-05 17:10:57 +0545

    exception handling for smooth

--- src/main/java/com/example/exceptions/GlobalExceptions.java
+++ src/main/java/com/example/exceptions/GlobalExceptions.java
@@ -1,16 +1,22 @@
 package com.example.exceptions;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice   //spring security lai vaneko yeta exception handle vanera
 public class GlobalExceptions {
 
+	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorDetails> otherExceptionHandler(Exception ue, 
 			WebRequest req){
+		ErrorDetails error = new ErrorDetails(ue.getMessage(), req.getDescription(false)
+				,LocalDateTime.now());
 		
-		
-		return null;
+		return new ResponseEntity<ErrorDetails>(error, HttpStatus.BAD_REQUEST);
 	}
 }
