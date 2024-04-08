254574f4376fc7ce8937ad217e81a4b8ea00ff72
SHIFU <tamangnischal2018@gmail.com>
2024-04-08 11:18:00 +0545

    cloudniry js

--- src/utils/uploadToCloudniry.js
+++ src/utils/uploadToCloudniry.js
@@ -1,1 +1,16 @@
 const cloud_name="dmjfctlgr";
+const upload_preset="nhhvi9rk";
+export const uploadToCloudniry=async(pics, fileType)=>{
+
+    if(pics && fileType){
+const data = new FormData()
+data.append("file", pics)
+data.append("upload_preset", upload_preset)
+data.append("cloud_name", cloud_name)
+cloud res = await fetch(`POST https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`)
+    }
+    else{
+        console.log("error........ayo")
+    }
+
+}
