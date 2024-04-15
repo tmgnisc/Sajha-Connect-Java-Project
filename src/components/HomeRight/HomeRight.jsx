1c489cd55380df20c7571bfce94bdfef8b6c2309
SHIFU <tamangnischal2018@gmail.com>
2024-04-15 12:29:20 +0545

    suggestion completed homeright

--- src/components/HomeRight/HomeRight.jsx
+++ src/components/HomeRight/HomeRight.jsx
@@ -3,23 +3,21 @@ import SearchUser from "../SearchUser/SearchUser";
 import PopularUserCard from "./PopularUserCard";
 import { Card } from "@mui/material";
 
-const popularUser = [1, 1, 1, 1, 1];
 const HomeRight = () => {
   return (
     <div className="pr-5">
       <SearchUser />
 
-   <Card className="p-5">
-   <div className="flex justify-between py-5 items-center">
-        <p className="font-semibold opacity-70">Suggestions for you</p>
-        <p className="text-xs font-semibold opacity-95">View All</p>
-      </div>
+      <Card className="p-5">
+        <div className="flex justify-between py-5 items-center">
+          <p className="font-semibold opacity-70">Suggestions for you</p>
+          <p className="text-xs font-semibold opacity-95">View All</p>
+        </div>
 
-      <div className="space-y-1">
-      {popularUser.map((item)=><PopularUserCard/>)}
-      </div>
-   </Card>
-  
+        <div className="space-y-1">
+          <PopularUserCard />
+        </div>
+      </Card>
     </div>
   );
 };
--- src/components/HomeRight/PopularUserCard.jsx
+++ src/components/HomeRight/PopularUserCard.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
-import {Avatar, Button, CardHeader, IconButton} from '@mui/material'
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import React, { useState, useEffect } from 'react';
+import { Avatar, Button, CardHeader } from '@mui/material';
+import axios from 'axios';
 import { red } from '@mui/material/colors';
 
 const PopularUserCard = () => {
+  const [popularUsers, setPopularUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchPopularUsers = async () => {
+      try {
+        const response = await axios.get('http://localhost:5454/users');
+        setPopularUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching popular users:', error);
+      }
+    };
+
+    fetchPopularUsers();
+  }, []);
+
   return (
     <div>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            
-          </Avatar>
-        }
-        action={
-         <Button size='small'>
-            Follow
-         </Button>
-        }
-        title="Nischal Tamang"
-        subheader="@nschal"
-      />
+      {popularUsers.map((user) => (
+        <CardHeader
+          key={user.id}
+          avatar={<Avatar sx={{ bgcolor: red[500] }} aria-label="recipe" />}
+          action={<Button size="small">Follow</Button>}
+          title={`${user.firstName} ${user.lastName}`}
+          subheader={`@${user.firstName.toLowerCase()}_${user.lastName.toLowerCase()}`}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default PopularUserCard
+export default PopularUserCard;
